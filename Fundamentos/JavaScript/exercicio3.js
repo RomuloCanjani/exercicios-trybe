6056b81fd76988feccd7280c304dd6870b0f8074
Romulo Canjani <romulocanjani@gmail.com>
2022-11-25 16:05:43 -0300

    exercicio3

---
+++ Fundamentos/JavaScript/exercicio3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultsum = 0
+for (let index = 0; index < numbers.length; index += 1) {
+    resultsum += numbers[index]    
+}
+
+let aritmetica = resultsum / numbers.length
+
+console. log(aritmetica)
