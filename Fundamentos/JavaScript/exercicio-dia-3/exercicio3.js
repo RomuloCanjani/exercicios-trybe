cf8463b65b6ed80b2e2dc32d65a97aec505c16e6
Romulo Canjani <romulocanjani@gmail.com>
2022-11-29 16:16:52 -0300

    exercicio3

---
+++ Fundamentos/JavaScript/exercicio-dia-3/exercicio3.js
@@ -0,0 +1,18 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = array[0];
+let menor = array[0];
+
+for (let index = 0; index <array.length ;index += 1) {
+    if (array[index].length > maior.length) {
+        maior = array[index];
+    }
+}
+console.log(maior);
+
+
+for (let index = 0; index <array.length; index +=1) {
+    if (array[index].length < menor.length) {
+        menor = array[index];
+    }
+}
+console.log(menor);
